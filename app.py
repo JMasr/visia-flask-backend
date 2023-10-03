fe42206dcdc9f76d0b41d0faeba88aa2d80493b2
José M. Ramírez <32576858+JMasr@users.noreply.github.com>
2023-10-03 20:53:48 +0200

    1. Refactoring the log functions for a better code. 2. Adding the Data Handler Section to Upload and Download videos.

--- app.py
+++ app.py
@@ -1,19 +1,35 @@
+import os
+
 from flask import Flask, request
 from flask_cors import CORS
 from flask_mongoengine import MongoEngine
+from flask_uploads import UploadSet, configure_uploads, ARCHIVES
 from pymongo.errors import DuplicateKeyError
 
 from config.backend_config import BasicMongoConfig
-from database.basic_mongo import LogActionsMongoDB
+from database.basic_mongo import LogActionsMongoDB, VideoActionsMongoDB
 from frontData.basic_record_types import BasicRecordSessionData
 from log.basic_log_types import LogOrigins
 from responses.basic_responses import DataResponse, BasicResponse
+from utils.utils import get_now_standard
+
+# from flask_jwt_extended import JWTManager
+# from security.basic_security import BasicSecurityHandler
 
 # Initialize Flask
 app = Flask(__name__)
 # Enable CORS for all routes
 CORS(app)
 
+# Set the secret key to enable JWT authentication
+# security_config = BasicSecurityConfig(secret_key='visia@sergas')
+# app.config['JWT_SECRET_KEY'] = security_config.secret_key
+# jwt = JWTManager(app)
+
+# security_handler = BasicSecurityHandler(security_config.secret_key)
+
+
+# Sample user data (in a real application, this would come from a database)
 # Configure MongoDB
 mongo_config = BasicMongoConfig(db='visia_demo', username='admin', password='admin')
 app.config['MONGODB_SETTINGS'] = mongo_config.model_dump()
@@ -22,6 +38,7 @@ mongo = MongoEngine()
 mongo.init_app(app)
 
 
+# Error handlers
 @app.errorhandler(404)
 def resource_not_found():
     """
@@ -40,34 +57,71 @@ def resource_not_found(e):
     return response.model_dump_json()
 
 
+# Endpoints
+
+# Index
 @app.route('/')
 def index():  # put application's code here
     return 'Welcome to the backend!'
 
 
-# Log Section
-# Endpoint to log data from the frontend
-@app.route('/log/addLogFrontEnd', methods=['POST'])
-def add_log_frontend() -> str:
-    """
-    Endpoint to log data from the FrontEnd
-    :return: A JSON object with a message and a status code.
-    """
-    # Get data from request
-    data = request.get_json()
+# Security Section
+# @app.route('/getAccessTokenBySecret', methods=['POST'])
+# def get_access_token() -> str:
+#     """
+#     Endpoint to get an access token.
+#     :return: A JSON object with an access token or a message and a status code.
+#     """
+#     # Get data from request
+#     data = request.get_json()
+#     try:
+#         # Get data from request body
+#         secret = data.get('secret')
+#         response = security_handler.getAccessToken(secret)
+#     except Exception as e:
+#         response = BasicResponse(success=False, status_code=500, message=str(e))
+#
+#     return response.model_dump_json()
 
+
+# Log Section
+def add_log(log_origin: str, log_type: str, message: str) -> BasicResponse:
+    """
+    Function to log data to MongoDB
+    :param log_origin: Origin of the log.
+    :param log_type: Type of the log.
+    :param message: Message of the log.
+    :return: a BasicResponse object with a message and a status code.
+    """
     try:
         # Get data from request body
-        log_origin = data.get('origin', LogOrigins.FRONTEND)  # Default to backend if not provided
-        log_type = data.get('log_type')
-        message = data.get('message')
-
-        # Create a Log Action
         log_action = LogActionsMongoDB(log_origin=log_origin, log_type=log_type, message=message)
         # Save the log in the database
         response = log_action.insert_log()
     except ValueError as e:
         response = BasicResponse(success=False, status_code=400, message=f'Invalid Value: {e}')
+    except Exception as e:
+        response = BasicResponse(success=False, status_code=500, message=str(e))
+    return response
+
+
+# Endpoint to log data from the frontend
+@app.route('/log/addLogFrontEnd', methods=['POST'])
+def upload_log_frontend() -> str:
+    """
+    Endpoint to log data from the FrontEnd
+    :return: A JSON object with a message and a status code.
+    """
+    try:
+        # Get data from request
+        data = request.get_json()
+        # Get data from request body
+        log_origin = LogOrigins.FRONTEND
+        log_type = data.get('log_type')
+        message = data.get('message')
+
+        # Add the log
+        response = add_log(log_origin=log_origin.value, log_type=log_type, message=message)
     except Exception as e:
         response = BasicResponse(success=False, status_code=500, message=str(e))
 
@@ -76,26 +130,20 @@ def add_log_frontend() -> str:
 
 
 @app.route('/log/addLogBackEnd', methods=['POST'])
-def add_log_backend() -> str:
+def upload_log_backend() -> str:
     """
     Endpoint to log data from the BackEnd
     :return: A JSON object with a message and a status code.
     """
-    # Get data from request
-    data = request.get_json()
-
     try:
+        # Get data from request
+        data = request.get_json()
         # Get data from request body
-        log_origin = data.get('origin', LogOrigins.BACKEND)  # Default to backend if not provided
+        log_origin = LogOrigins.BACKEND
         log_type = data.get('log_type')
         message = data.get('message')
-
-        # Create a Log Action
-        log_action = LogActionsMongoDB(log_origin=log_origin, log_type=log_type, message=message)
-        # Save the log in the database
-        response = log_action.insert_log()
-    except ValueError as e:
-        response = BasicResponse(success=False, status_code=400, message=f'Invalid Value: {e}')
+        # Add the log
+        response = add_log(log_origin=log_origin.value, log_type=log_type, message=message)
     except Exception as e:
         response = BasicResponse(success=False, status_code=500, message=str(e))
 
@@ -105,17 +153,16 @@ def add_log_backend() -> str:
 
 # Endpoint to retrieve logs by type
 @app.route('/log/getLogsBy', methods=['GET'])
-def get_logs_by_type() -> str:
+def get_logs_by() -> str:
     """
     Endpoint to retrieve logs from the MongoDB database based on specified filters.
     We use a dictionary containing filter criteria, if one of the is empty we ignore and use the others, e.g.,
      {'log_type': 'DEBUG', 'log_origin': 'BACKEND'., 'id': '096asdf'}
     :return: A JSON object with a list of logs or a message and a status code.
     """
-    # Get data from request
-    data = request.args
-
     try:
+        # Get data from request
+        data = request.args
         # Get data from request body
         query = {key: value for key, value in data.items() if value != ""}
         # Create a Log Action
@@ -128,7 +175,6 @@ def get_logs_by_type() -> str:
 
 
 # Render functions for the frontend
-## Render the Record-Session data
 @app.route('/getRecordData')
 def get_record_session_data():
     try:
@@ -141,6 +187,96 @@ def get_record_session_data():
         response = DataResponse(success=False, status_code=400, message=f"Error: {e}", data={})
 
     return response.model_dump()
+
+
+# Data Handler Section
+
+# Configure file uploads
+upload_files = os.path.join(os.getcwd(), 'uploads')
+app.config['UPLOADED_DEFAULT_DEST'] = upload_files  # Change this to your desired upload directory
+app.config['UPLOADED_DEFAULT_URL'] = 'http://localhost:5000/uploads/'  # Change this to your server's URL
+app.config['UPLOADED_DEFAULT_ALLOW'] = set(ARCHIVES)
+app.config['UPLOADED_DEFAULT_DENY'] = set()
+uploads = UploadSet('default', extensions=('',))
+configure_uploads(app, (uploads,))
+
+
+@app.route('/video/uploads', methods=['POST'])
+def upload_video() -> str:
+    """
+    Endpoint to upload a video file to the server.
+    :return:
+    """
+    try:
+        # Get the video file from the request
+        video = request.files['video']
+
+        # # Save the video file to a specified path
+        # save = os.path.join(upload_files, video.filename)
+        # video.save(save)
+
+        # Get the data from the request body
+        crd_id = request.form.get('crd_id', "UNK")
+        patient_id = request.form.get('patient_id', "UNK")
+        file_name = request.form.get('file_name', f"{crd_id}_{patient_id}--{get_now_standard()}.wbm")
+
+        # Create a VideoAction
+        video_act = VideoActionsMongoDB(crd_id=crd_id, patient_id=patient_id, filename=file_name, file=video)
+        # Save the video path to MongoDB
+        response = video_act.insert_video(video)
+
+        # Log the video upload
+        if response.success:
+            add_log(log_origin=LogOrigins.BACKEND.value, log_type="INFO", message=f"Video uploaded: {file_name}")
+        else:
+            add_log(log_origin=LogOrigins.BACKEND.value, log_type="ERROR", message=response.message)
+
+    except Exception as e:
+        # Add a log
+        add_log(log_origin=LogOrigins.BACKEND.value, log_type="ERROR", message=str(e))
+        response = BasicResponse(success=False, status_code=500, message=str(e))
+
+    return response.model_dump_json()
+
+
+@app.route('/video/downloadBy', methods=['GET'])
+def download_video():
+    """
+    Endpoint to download a video file from the server.
+    :return:
+    """
+    try:
+        # Get tghe data from the request
+        data = request.args
+        # Get data from request body
+        query = {key: value for key, value in data.items() if value != ""}
+        # Create a VideoAction
+        videos_obj = VideoActionsMongoDB.get_videos_by(query)
+
+        # TODO: Create a function to download all the videos on videos_obj
+        if videos_obj.success and videos_obj.data:
+            #     # Get the video file
+            #     video_file = [video.get("file") for video in videos_obj.data]
+            #
+            #     # Write the video file to a specified path
+            #     if not os.path.exists(upload_files):
+            #         os.makedirs(upload_files)
+            #
+            #     file_name = 'video.webm'
+            #     file_path = os.path.join(upload_files, file_name)
+            #     with open(file_path, 'wb') as f:
+            #         f.write(video_file[0])
+
+            response = BasicResponse(success=True, status_code=200, message="Video downloaded successfully")
+        else:
+            response = BasicResponse(success=False, status_code=400, message="Video/s not found")
+
+    except Exception as e:
+        # Add a log
+        add_log(log_origin=LogOrigins.BACKEND.value, log_type="ERROR", message=str(e))
+        response = BasicResponse(success=False, status_code=500, message=str(e))
+
+    return response.model_dump_json()
 
 
 if __name__ == '__main__':
