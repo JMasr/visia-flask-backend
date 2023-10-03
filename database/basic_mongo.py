956b5eec7c4859a3f6e1b4c7bf111f5b70177f40
José M. Ramírez <32576858+JMasr@users.noreply.github.com>
2023-10-03 20:52:09 +0200

    Modeling the Video Object in 2 classes: 1. VideoDocument: an object to represent a video with its metadata 2. VideoActionsMongoDB: an object to interact with VideoDocument and MongoDB (insertions, queries, etc)

--- database/basic_mongo.py
+++ database/basic_mongo.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from mongoengine import Document, StringField, DateTimeField, EnumField
+from mongoengine import Document, StringField, DateTimeField, EnumField, FileField
 from pydantic import BaseModel
+from werkzeug.datastructures import FileStorage
 
 from log.basic_log_types import LogOrigins, LogTypes
 from responses.basic_responses import BasicResponse, DataResponse, ListResponse
@@ -19,6 +20,23 @@ class LogDocument(Document):
     log_origin = EnumField(LogOrigins, required=True)
     message = StringField(required=True)
     timestamp = DateTimeField(default=datetime.now())
+
+
+class VideoDocument(Document):
+    # Metadata
+    crd_id = StringField(required=True)
+    patient_id = StringField(required=True)
+    timestamp = DateTimeField(default=datetime.now())
+    # Video
+    filename = StringField(required=True)
+    _file = FileField(required=True)
+
+    def get_video(self):
+        """
+        Get the file from the VideoDocument.
+        :return: A binary file.
+        """
+        return self._file.read()
 
 
 # MongoDB actions
@@ -51,7 +69,7 @@ class LogActionsMongoDB(BaseModel):
             response = BasicResponse(success=False, status_code=400, message=f'Invalid Value: {e}')
         except Exception as e:
             response = BasicResponse(success=False, status_code=500, message=str(e))
-
+        # Return the response
         return response
 
     @staticmethod
@@ -63,8 +81,7 @@ class LogActionsMongoDB(BaseModel):
         """
         try:
             # Retrieve logs based on the query from MongoDB
-            logs_query = {key: value for key, value in query.items()}
-            logs = LogDocument.objects(**logs_query)
+            logs = LogDocument.objects(**query)
 
             log_data = [
                 {"id": str(log.id), "log_type": log.log_type, "message": log.message, "log_origin": log.log_origin}
@@ -73,7 +90,73 @@ class LogActionsMongoDB(BaseModel):
 
         except Exception as e:
             response = BasicResponse(success=False, status_code=500, message=str(e))
-
+        # Return the response
         return response
 
 
+class VideoActionsMongoDB(BaseModel):
+    """
+    Class to perform actions with Videos on the MongoDB database.
+    :param crd_id: Receive the CRD ID of the video.
+    :param patient_id: Receive the Patient ID of the video.
+    :param filename: Receive the filename of the video.
+    """
+    # Metadata
+    crd_id: str
+    patient_id: str
+    # Video
+    filename: str
+
+    def insert_video(self, file: FileStorage) -> BasicResponse:
+        """
+        Insert a Video Object in the MongoDB database.
+        :return: A JSON object with a message and a status code.
+        """
+        try:
+            if file:
+                # Create a Video Document
+                new_video = VideoDocument(crd_id=self.crd_id, patient_id=self.patient_id, filename=self.filename,
+                                          _file=file)
+                # Save the video in the database
+                id_mongo_db = new_video.save()
+                id_video = str(id_mongo_db.id)
+
+                # Check if the video has the correct parameters
+                if self.crd_id == "UNK" or self.patient_id == "UNK" or "UNK" in self.filename:
+                    # Handle the case where the video has missing parameters
+                    response = DataResponse(success=True, status_code=199,
+                                            essage="Video uploaded, but missing parameters", data={"id": id_video})
+                else:
+                    # Handle the case where the video has all the parameters
+                    response = DataResponse(success=True, status_code=200, message="Video added: successfully",
+                                            data={"id": id_video})
+            else:
+                # Handle the case where no file was received
+                response = BasicResponse(success=False, status_code=400, message="No file received")
+        except ValueError as e:
+            # Handle the case where the received file is not a video
+            response = BasicResponse(success=False, status_code=400, message=f'Invalid Value: {e}')
+        except Exception as e:
+            # Handle the case where an error occurred
+            response = BasicResponse(success=False, status_code=500, message=str(e))
+        # Return the response
+        return response
+
+    @staticmethod
+    def get_videos_by(query: dict[str, str]) -> BasicResponse:
+        """
+        Get videos from the MongoDB database based on specified filters.
+        :param query: A dictionary containing filter criteria, e.g., {'patient_id': '123456789'}
+        :return: A JSON object with a message and a status code.
+        """
+        try:
+            # Create a Video Document
+            videos = VideoDocument.objects(**query)
+            video_data = [{"id": str(video.id), "crd_id": video.crd_id, "patient_id": video.patient_id,
+                           "filename": video.filename, "file": video.get_video()} for video in videos]
+            response = ListResponse(success=True, status_code=200, message="Videos retrieved successfully",
+                                    data=video_data)
+        except Exception as e:
+            response = BasicResponse(success=False, status_code=500, message=str(e))
+        # Return the response
+        return response
