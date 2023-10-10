34f3ab3efbec3a4beb6ccdbcabfbc14c6a041f9b
José M. Ramírez <moscaelectronica@gmail.com>
2023-10-10 12:01:46 +0200

    Adding an utils module

---
+++ utils/utils.py
@@ -0,0 +1,9 @@
+from datetime import datetime
+
+
+def get_now_standard() -> str:
+    """
+    Get the current date and time in a standard format.
+    :return: A string with the current date and time in the standard format.
+    """
+    return datetime.now().strftime("%d/%m/%y-%H-%M-%S")
