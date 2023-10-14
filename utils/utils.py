d264d8f811f1d990c17a83fff307456a80923f96
José M. Ramírez <moscaelectronica@gmail.com>
2023-10-14 12:07:40 +0200

    Improving the standard format of date to avoid problems with the Windows´s file system

--- utils/utils.py
+++ utils/utils.py
@@ -6,4 +6,4 @@ def get_now_standard() -> str:
     Get the current date and time in a standard format.
     :return: A string with the current date and time in the standard format.
     """
-    return datetime.now().strftime("%d/%m/%y-%H-%M-%S")
+    return datetime.now().strftime("%d-%m-%y_%H-%M-%S")
