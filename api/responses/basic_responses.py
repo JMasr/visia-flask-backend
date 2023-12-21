b63de98e1435abed9615fd0ab8025276e147a720
José M. Ramírez <moscaelectronica@gmail.com>
2023-12-21 16:32:21 +0100

    Adding an inner method to logger BasicResponses natively

--- api/responses/basic_responses.py
+++ api/responses/basic_responses.py
@@ -1,10 +1,21 @@
 from pydantic import BaseModel
 
+from api.log import app_logger
+
 
 class BasicResponse(BaseModel):
     success: bool
     status_code: int
     message: str
+
+    def log_response(self, module: str, action: str):
+        """
+        Log the response details.
+        """
+        app_logger.info(
+            f"127.0.0.1 - {module} - {action} - Success: {self.success} - "
+            f"Message: {self.message} - Status Code: {self.status_code}"
+        )
 
 
 class ListResponse(BasicResponse):
